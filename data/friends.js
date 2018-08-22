3041a6491c675bb5f2b6abdd8d232285b3d370bb
Durian <karen.kua7@gmail.com>
2018-08-22 17:24:59 -0400

    Added more data to friends Array

--- data/friends.js
+++ data/friends.js
@@ -30,8 +30,39 @@ var tableArr = [
       4,
       1
     ]
-
-  }
+  }, 
+  {
+    "name":"Jolie",
+    "photo":"https://media.licdn.com/mpr/mpr/shrinknp_400_400/p/6/005/064/1bd/3435aa3.jpg",
+    "scores":[
+        4,
+        2,
+        3,
+        4,
+        4,
+        2,
+        3,
+        4,
+        1,
+        2
+      ]
+    }, 
+    {
+      "name":"Carl",
+      "photo":"https://media.licdn.com/mpr/mpr/shrinknp_400_400/p/6/005/064/1bd/3435aa3.jpg",
+      "scores":[
+          1,
+          4,
+          2,
+          2,
+          4,
+          2,
+          3,
+          2,
+          4,
+          1
+        ]
+      }
 ]
 
 // Note how we export the array. This makes it accessible to other files using require.
