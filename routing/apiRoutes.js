72d144ec43481844b606a859e18d315ec1f9a942
Durian <karen.kua7@gmail.com>
2018-08-22 01:48:08 -0400

    test

--- routing/apiRoutes.js
+++ routing/apiRoutes.js
@@ -5,11 +5,6 @@
 // ===============================================================================
 
 var friends = require("../data/friends");
-var bodyParser = require("body-parser");
-
-app.listen(PORT, function() {
-  console.log("App listening on PORT " + PORT);
-});
 
 // ===============================================================================
 // ROUTING
