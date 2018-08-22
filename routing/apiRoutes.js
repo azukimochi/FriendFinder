641f1dfe8360f33fa757bbebc2b1a062024e5596
Durian <karen.kua7@gmail.com>
2018-08-22 01:52:07 -0400

    test

--- routing/apiRoutes.js
+++ routing/apiRoutes.js
@@ -29,7 +29,7 @@ module.exports = function(app) {
   // Then the server saves the data to the tableData array)
   // ---------------------------------------------------------------------------
 
-  app.post("/api/friends", function(req, res) {
+  app.post("/survey", function(req, res) {
 
     // Note the code here. Our "server" will respond to requests and let users know if they have a table or not.
     // It will do this by sending out the value "true" have a table
